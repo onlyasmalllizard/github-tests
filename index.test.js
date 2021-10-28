9b09dd4f56b6131a5f564d910fa24a81a0415a10
Lizard Morrow <lizardmorrow@gmail.com>
2021-10-28 09:32:03 +0100

    Add second test

--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ const { addTwoNumbers } = require('./index');
 test('adds 1 + 2 to equal 3', () => {
   expect(addTwoNumbers(1, 2)).toBe(3);
 });
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(addTwoNumbers(2, 3).toBe(5));
+});
